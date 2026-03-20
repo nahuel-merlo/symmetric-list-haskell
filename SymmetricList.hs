e849ae4147ff97d328f7e2ba39f60d0bd6ef1c64
nahuepera5 <n.peraza04@gmail.com>
2026-03-20 19:03:50 -0300

    (feat): implement Functor instance

--- SymmetricList.hs
+++ SymmetricList.hs
@@ -10,61 +10,69 @@ import qualified Data.List as Lst
     null ys = single xs || null xs
 --}
 
-type SymmetricList a = ([a], [a])
+newtype SymmetricList a = SL ([a], [a]) 
 
 init , fromList :: [a] -> SymmetricList a
 init = fromList
-fromList l = (xs, Lst.reverse ys) 
+fromList l = SL (xs, Lst.reverse ys) 
     where
         (xs, ys) = Lst.splitAt (Lst.length l `div` 2) l
 
-fromSymetricList :: SymmetricList a -> [a]
-fromSymetricList (xs, ys) = xs ++ reverse ys
+toList :: SymmetricList a -> [a]
+toList (SL (xs, ys)) = xs ++ reverse ys
 
 nil :: SymmetricList a
-nil = ([], [])
+nil = SL ([], [])
 
 cons :: a -> SymmetricList a -> SymmetricList a
-cons x (xs, ys)
-    | Lst.null ys = (ys, [x])
-    | otherwise   = (x:xs, ys)
+cons x (SL (xs, ys))
+    | Lst.null ys = SL (ys, [x])
+    | otherwise   = SL (x:xs, ys)
 
 snoc :: a -> SymmetricList a -> SymmetricList a
-snoc y (xs, ys)
-    | Lst.null xs = (ys, [y])
-    | otherwise   = (xs, y:ys)
+snoc y (SL (xs, ys))
+    | Lst.null xs = SL (ys, [y])
+    | otherwise   = SL (xs, y:ys)
 
 head :: SymmetricList a -> a
-head ([], [])    = error "head on empty symmetric list"
-head ((x:xs), _) = x
-head (_, ys)     = (Lst.head . Lst.reverse) ys
+head (SL ([], []))    = error "head on empty symmetric list"
+head (SL ((x:xs), _)) = x
+head (SL (_, ys))     = (Lst.head . Lst.reverse) ys
 
 last :: SymmetricList a -> a
-last ([], [])    = error "last on empty symmetric list"
-last (_, (y:ys)) = y
-last (xs, _)     = (Lst.head . Lst.reverse) xs
+last (SL ([], []))    = error "last on empty symmetric list"
+last (SL (_, (y:ys))) = y
+last (SL (xs, _))     = (Lst.head . Lst.reverse) xs
 
 tail :: SymmetricList a -> SymmetricList a
-tail ([], [])  = error "tail on empty symmetric list"
-tail ([], [y]) = nil
-tail (xs, ys)
-    | (length xs) == 1 = (reverse vs, us)
-    | otherwise        = (Lst.tail xs, ys)
+tail (SL ([], []))  = error "tail on empty symmetric list"
+tail (SL ([], [y])) = nil
+tail (SL (xs, ys))
+    | (length xs) == 1 = SL (reverse vs, us)
+    | otherwise        = SL (Lst.tail xs, ys)
     where
         (us, vs) = Lst.splitAt ((Lst.length ys) `div` 2) ys
 
 dropWhile :: (a -> Bool) -> SymmetricList a -> SymmetricList a
-dropWhile p sl@(xs, ys) 
+dropWhile p sl@(SL (xs, ys)) 
     | p $ Lst.head xs = dropWhile p (tail sl)
     | otherwise       = sl
 
 null :: SymmetricList a -> Bool
-null ([], []) = True
-null _        = False
+null (SL ([], [])) = True
+null _           = False
 
 single :: SymmetricList a -> Bool
-single ([x], []) = True
-single ([], [y]) = True
-single _         = False
+single (SL ([x], [])) = True
+single (SL ([], [y])) = True
+single _            = False
 
-{-- Test values --}
+{-- Instances --}
+
+instance Show a => Show (SymmetricList a) where
+    -- show (SL l) = show l
+    -- show sl = show $ toList sl 
+    show (SL (xs, ys)) = ("<- ") ++ (show xs) ++ (" | ") ++ (show $ reverse ys) ++ (" ->") 
+
+instance Functor SymmetricList where
+    fmap f (SL (xs, ys)) = SL ([f x | x <- xs], [f y | y <- ys]) 
