5ec2a1c5d5249c4bf3663262deb0999efbe5aaf9
nahuepera5 <n.peraza04@gmail.com>
2026-09-11 17:41:07 -0300

    (test): implement manual tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,15 @@
 module Main (main) where
 
+import Data.SymmetricList
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    test "fromList empty" $ toList (fromList ([] :: [Int])) == []
+    test "fromList" $ toList (fromList [1 .. 1000]) == [1 .. 1000] 
+    test "const" $ toList (cons 1 $ fromList [2 .. 1000]) == 1:[2 .. 1000]
+    test "snoc" $ toList (snoc 1000 $ fromList [1 .. 999]) == [1 .. 1000]
+
+test :: String -> Bool -> IO ()
+test name condition
+    | condition = putStrLn ("✓ " ++ name)
+    | otherwise = putStrLn ("✗ " ++ name)
