55d4936719220a82b760236d2ef5108ed961ed19
nahuepera5 <n.peraza04@gmail.com>
2026-09-11 17:56:55 -0300

    (test): implement hspec tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,19 @@
 module Main (main) where
 
+import Test.Hspec
 import Data.SymmetricList
 
 main :: IO ()
-main = do
-    test "fromList empty" $ toList (fromList ([] :: [Int])) == []
-    test "fromList" $ toList (fromList [1 .. 1000]) == [1 .. 1000] 
-    test "const" $ toList (cons 1 $ fromList [2 .. 1000]) == 1:[2 .. 1000]
-    test "snoc" $ toList (snoc 1000 $ fromList [1 .. 999]) == [1 .. 1000]
+main = hspec $ do
+    describe "fromList" $ do
+        it "creates an empty list from []" $ do
+            toList (fromList ([] :: [Int])) `shouldBe` []
+        
+        it "preserve the elements" $ do 
+            toList (fromList [1 .. 1000]) `shouldBe` [1 .. 1000]
 
-test :: String -> Bool -> IO ()
-test name condition
-    | condition = putStrLn ("✓ " ++ name)
-    | otherwise = putStrLn ("✗ " ++ name)
+    describe "cons" $ do 
+        it "add an element to an empty list" $ do
+            toList (cons 1 $ fromList []) `shouldBe` [1]
+        it "add an element" $ do
+            toList (cons 1 $ fromList [2 .. 1000]) `shouldBe` 1:[2 .. 1000]
