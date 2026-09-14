eab6cabbf3389428ca66ca4be9872f7cbea1e517
nahuepera5 <n.peraza04@gmail.com>
2026-09-14 00:41:40 -0300

    (test): add tests for the snoc function

--- test/Main.hs
+++ test/Main.hs
@@ -24,3 +24,10 @@ main = hspec $ do
             SL.toList (SL.cons 1 $ SL.nil) `shouldBe` 1:[]
         it "add an element" $ do
             SL.toList (SL.cons 1 $ SL.fromList [2 .. 3]) `shouldBe` 1:[2 .. 3]
+
+    describe "snoc" $ do
+        it "add an element to an empty list" $ do
+            SL.toList (SL.snoc 1 SL.nil) `shouldBe` [] ++ [1]
+
+        it "add an element to the back" $ do
+            SL.toList (SL.snoc 3 $ SL.fromList [1 .. 2]) `shouldBe` ([1 .. 2] ++ [3])
