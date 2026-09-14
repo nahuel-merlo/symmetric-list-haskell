b41812b21bbb75fa9578ab737806a85b85860373
nahuepera5 <n.peraza04@gmail.com>
2026-09-14 00:44:10 -0300

    (test): add tests for the head function

--- test/Main.hs
+++ test/Main.hs
@@ -31,3 +31,16 @@ main = hspec $ do
 
         it "add an element to the back" $ do
             SL.toList (SL.snoc 3 $ SL.fromList [1 .. 2]) `shouldBe` ([1 .. 2] ++ [3])
+
+    describe "head" $ do
+        it "trhow error when the list is empty" $ do
+            SL.head SL.nil `shouldThrow` anyErrorCall
+
+        it "give the first element in the list" $ do
+            SL.head (SL.fromList [1 .. 3]) `shouldBe` L.head [1 .. 3]
+
+        it "take out the element we just added from the top" $ do
+            SL.head (SL.cons 1 SL.nil) `shouldBe` L.head (1:[])
+
+        it "take out the element we just added from the bottom" $ do
+            SL.head (SL.snoc 1 SL.nil) `shouldBe` L.head (1:[])
