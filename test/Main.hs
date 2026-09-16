14283615308d845be6f126cce0dc072c929bf7d8
nahuepera5 <n.peraza04@gmail.com>
2026-09-16 20:08:46 -0300

    (test): use property based testing and remove unnecessary test

--- test/Main.hs
+++ test/Main.hs
@@ -1,62 +1,79 @@
 module Main (main) where
 
 import Test.Hspec 
+import Test.QuickCheck
 import qualified Data.SymmetricList as SL
 import qualified Data.List as L
 import Control.Exception (evaluate)
 
+prop_fromList_toList :: [Int] -> Bool
+prop_fromList_toList xs =
+    SL.toList (SL.fromList xs) == xs
+
+prop_cons :: Int -> [Int] -> Bool
+prop_cons x xs = 
+    SL.toList (SL.cons x (SL.fromList xs)) == x:xs  
+
+prop_snoc :: Int -> [Int] -> Bool
+prop_snoc x xs = 
+    SL.toList (SL.snoc x (SL.fromList xs)) == xs ++ [x]
+
+prop_head :: [Int] -> Property
+prop_head xs = not (null xs) ==> SL.head (SL.fromList xs) == L.head xs
+
+prop_last :: [Int] -> Property
+prop_last xs = not (null xs) ==> SL.last (SL.fromList xs) == L.last xs
+
+prop_tail :: [Int] -> Property
+prop_tail xs = not (null xs) ==> SL.toList (SL.tail (SL.fromList xs)) == L.tail xs
+
+prop_null :: [Int] -> Bool
+prop_null xs = SL.null (SL.fromList xs) == L.null xs 
+
+prop_dropWhile :: Fun Int Bool -> [Int] -> Bool
+prop_dropWhile f xs = SL.toList (SL.dropWhile (applyFun f) (SL.fromList xs)) == L.dropWhile (applyFun f) xs
+
 main :: IO ()
 main = hspec $ do
     describe "fromList" $ do
-        it "creates an empty list from []" $ do
-            SL.toList (SL.fromList ([] :: [Int])) `shouldBe` []
+        it "round-trips a list through toList" $ do
+            property prop_fromList_toList
         
-        it "preserve the elements" $ do 
-            SL.toList (SL.fromList [1 .. 3]) `shouldBe` [1 .. 3]
-
-    describe "toList" $ do
-        it "creates from nil" $ do
-            ((SL.toList SL.nil) :: [Int]) `shouldBe` ([] :: [Int])
-        
-
     describe "cons" $ do 
-        it "behaves like (:) on empty lists" $ do
-            SL.toList (SL.cons 1 $ SL.nil) `shouldBe` 1:[]
         it "behaves like (:) on lists" $ do
-            SL.toList (SL.cons 1 $ SL.fromList [2 .. 3]) `shouldBe` 1:[2 .. 3]
+            property prop_cons
 
     describe "snoc" $ do
-        it "behaves like (++ [x]) on empty lists" $ do
-            SL.toList (SL.snoc 1 SL.nil) `shouldBe` [] ++ [1]
 
         it "behaves like (++ [x]) on lists" $ do
-            SL.toList (SL.snoc 3 $ SL.fromList [1 .. 2]) `shouldBe` ([1 .. 2] ++ [3])
+            property prop_snoc
 
     describe "head" $ do
         it "trhows an error when the list is empty" $ do
             SL.head SL.nil `shouldThrow` anyErrorCall
-
-        it "behaves like head on lists" $ do
-            SL.head (SL.fromList [1 .. 3]) `shouldBe` L.head [1 .. 3]
 
         it "take out the element we just added from the top" $ do
             SL.head (SL.cons 1 SL.nil) `shouldBe` L.head (1:[])
 
         it "take out the element we just added from the bottom" $ do
             SL.head (SL.snoc 1 SL.nil) `shouldBe` L.head (1:[])
+        
+        it "behaves like head on lists" $ do
+            property prop_head
 
     describe "last" $ do
         it "throws an error when the list is empty" $ do
             SL.last SL.nil `shouldThrow` anyErrorCall
-
-        it "behaves like last on lists" $ do
-            SL.last (SL.fromList [1 .. 3]) `shouldBe` last [1 .. 3]
 
         it "give the last element we just added from the front" $ do
             SL.last (SL.cons 1 SL.nil) `shouldBe` L.last (1:[])
 
         it "give the last element we just added from the back" $ do
             SL.last (SL.snoc 1 SL.nil) `shouldBe` L.last (1:[])
+
+        it "behaves like last on lists" $ do
+            property prop_last 
+
 
     describe "tail" $ do
         it "throw error when the list is empty" $ do
@@ -69,17 +86,11 @@ main = hspec $ do
             SL.toList (SL.tail (SL.snoc 1 SL.nil)) `shouldBe` []
 
         it "give the tail of the list" $ do
-            SL.toList (SL.tail (SL.fromList [1 .. 10])) `shouldBe` L.tail [1 .. 10]
+            property prop_tail
 
     describe "null" $ do
         it "behaves like null on lists" $ do
-            SL.null SL.nil `shouldBe` L.null []
-
-        it "gives False when the list has one element" $ do
-            SL.null (SL.cons 1 SL.nil) `shouldBe` L.null (1:[])
-        
-        it "gives False when the list has n elemnts" $ do
-            SL.null (SL.fromList [1 .. 3]) `shouldBe` L.null [1 .. 3]
+            property prop_null
 
     describe "dropWhile" $ do
         it "give nil when the list is empty" $ do
@@ -92,4 +103,4 @@ main = hspec $ do
             SL.toList (SL.dropWhile (\x -> False) $ SL.fromList [1 .. 3]) `shouldBe` L.dropWhile (\x -> False) [1 .. 3]
 
         it "behaves like dropWhile on lists" $ do 
-            SL.toList (SL.dropWhile (==2) $ SL.fromList [1 .. 3]) `shouldBe` L.dropWhile (==2) [1 .. 3]
+            property prop_dropWhile
